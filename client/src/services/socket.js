162254dc29e1e66f6d277b61c6ca56d4f380a379
Ali-Atif07 <encodedecode10@gmail.com>
2026-02-08 19:32:20 +0530

    update env

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -5,7 +5,7 @@ let socket;
 export const getSocket = () => {
 
   if (!socket) {
-    const url = process.env.VITE_API_URL || 'http://localhost:5000';
+    const url = import.meta.env.VITE_API_URL || 'http://localhost:5000';
     socket = io(url, {
       transports: ['websocket'],
       reconnection: true,
