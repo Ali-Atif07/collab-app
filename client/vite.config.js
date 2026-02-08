75daea28db4e74f647dc480bb1746f3930e4d2e2
Ali-Atif07 <encodedecode10@gmail.com>
2026-02-08 19:39:12 +0530

    Fix proxy target assignment in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/socket.io': {
-        target:  import.meta.env.VITE_API_URL || 'http://localhost:5000',
+        target:   process.env.VITE_API_URL;,
         ws: true
       }
     }
